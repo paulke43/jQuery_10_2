e457d531f8f3a70487fe356f2a854b3bdbce7501
Pawel Morka <morka.paul@gmail.com>
2018-02-12 13:48:58 +0100

    Add jquery

---
+++ js/scripts.js
@@ -0,0 +1,19 @@
+//Changes css elem 
+var span = $("span");
+    span.each(function(index, element) {
+    	if(index % 2 == 0) {
+		$(element).css('color', 'red');
+	};
+});
+//Add button to each paragraphs
+var paragraphs = $('p');
+paragraphs.each(function(index, element) {
+
+    var button = '<button class="btn" data-tmp="' + index + '">Click me</button>'
+    $(element).append(button)
+
+});
+//Add event handler to each button
+$("button").click(function(){
+	alert($(this).attr("data-tmp"));
+});
